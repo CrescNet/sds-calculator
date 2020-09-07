60b26bfbc413458e5ef88e6b14f0ab54c3e259ea
Christoph Beger <cbeger@imise.uni-leipzig.de>
2020-09-07 11:16:55 +0200

    Refactor app into ui and server
    Add footer

--- server.R
+++ server.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(openxlsx)
 library(childsds)
@@ -25,7 +16,7 @@ calculateSdsValues <- function(
   female = 'female'
 ) {
   try({
-    ref    <- eval(parse(text = reference))
+    ref <- eval(parse(text = reference))
   }, silent = TRUE)
 
   try({
@@ -36,6 +27,9 @@ calculateSdsValues <- function(
 
   for (object in c('height', 'weight', 'bmi')) {
     try({
+      if (length(data[[column.names[[object]]]]) == 0) {
+        next
+      }
       data[[paste(column.names[[object]], 'SDS')]] <- sds(
         value  = data[[column.names[[object]]]],
         age    = data[[column.names$age]],
@@ -46,58 +40,14 @@ calculateSdsValues <- function(
         ref    = ref
       )
       data[[paste(column.names[[object]], 'P')]] <- pnorm(data[[paste(column.names[[object]], 'SDS')]]) * 100
-    }, silent = FALSE)
+    }, silent = TRUE)
   }
 
   return(data)
 }
 
-ui <- fluidPage(
-  titlePanel('SDS Calculator for Excel Sheets'),
 
-  # Description of the app
-  'Please choose an Excel file (XLSX) and modify the configurations as needed.',
-  'A table will be displayed with a preview of the resulting data.',
-  br(),
-  'When you are satisfied, click "Generate" to save the resulting Excel file.',
-  br(),
-
-  tags$h3('Disclaimer'),
-  'This tool is not approved as a medicinal product for clinical use, and should be used for research purposes only.',
-
-  hr(),
-
-  fluidRow(
-    column(3, fileInput('excel', 'Choose an XLSX File', accept = '.xlsx')),
-    column(
-      3,
-      selectInput(
-        'reference',
-        'Choose a reference',
-        c(
-          'Kromeyer-Hauschild' = 'kro.ref',
-          'WHO' = 'who.ref',
-          'KiGGS' = 'kiggs.ref',
-          'AGA' = 'aga_15.ref',
-          'Ethiopia' = 'ethiop.ref',
-          'Flanders, Belgium' = 'belgium.ref',
-          'Portugal' = 'portug.ref',
-          'UK WHO' = 'ukwho.ref',
-          'China' = 'zong13.ref',
-          'CDC' = 'cdc.ref'
-        )
-      )
-    ),
-    column(6, style = "margin-top: 25px", tagList('The R package ', a('childsds', href = 'https://cran.r-project.org/package=childsds'), 'is used to calculated the SDS values.'))
-  ),
-
-  hr(),
-
-  uiOutput('data_set_config')
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+shinyServer(function(input, output) {
   options(shiny.maxRequestSize = 30 * 1024 ^ 2)
 
   data <- reactive({
@@ -133,6 +83,7 @@ server <- function(input, output) {
   output$data_set_config <- renderUI({
     req(data())
     tagList(
+      hr(),
       fluidRow(
         column(3, varSelectInput('sex_col', 'Sex column', data())),
         column(3, varSelectInput('age_col', 'Age column (years)', data())),
@@ -173,8 +124,4 @@ server <- function(input, output) {
       write.xlsx(data, file)
     }
   )
-}
-
-# Run the application
-shinyApp(ui = ui, server = server)
-
+})
---
+++ ui.R
@@ -0,0 +1,50 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel('SDS Calculator for Excel Sheets'),
+
+  # Description of the app
+  'Please choose an Excel file (XLSX) and modify the configurations as needed.',
+  'A table will be displayed with a preview of the resulting data.',
+  br(),
+  'When you are satisfied, click "Generate" to save the resulting Excel file.',
+  br(),
+
+  tags$h3('Disclaimer'),
+  'This tool is not approved as a medicinal product for clinical use, and should be used for research purposes only.',
+
+  hr(),
+
+  fluidRow(
+    column(3, fileInput('excel', 'Choose an XLSX File', accept = '.xlsx')),
+    column(
+      3,
+      selectInput(
+        'reference',
+        'Choose a reference',
+        c(
+          'Kromeyer-Hauschild' = 'kro.ref',
+          'WHO' = 'who.ref',
+          'KiGGS' = 'kiggs.ref',
+          'AGA' = 'aga_15.ref',
+          'Ethiopia' = 'ethiop.ref',
+          'Flanders, Belgium' = 'belgium.ref',
+          'Portugal' = 'portug.ref',
+          'UK WHO' = 'ukwho.ref',
+          'China' = 'zong13.ref',
+          'CDC' = 'cdc.ref'
+        )
+      )
+    ),
+    column(6, style = "margin-top: 25px", tagList('The R package ', a('childsds', href = 'https://cran.r-project.org/package=childsds'), 'is used to calculated the SDS values.'))
+  ),
+
+  uiOutput('data_set_config'),
+
+  tags$footer(
+    hr(),
+      tags$small('Source code of this Shiny app is available at: ',
+      a('GitHub', href = 'https://github.com/CrescNet/sds-calculator')
+    ), align = 'center'
+  )
+))
